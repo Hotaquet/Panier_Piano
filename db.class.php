dec9ab011cbedf1cc385127c655226cf9faaae9d
Lucas Ribeiro <lucasribeiro7d@gmail.com>
2018-01-25 13:59:18 +0100

    Modification de db.class.php

--- db.class.php
+++ db.class.php
@@ -3,7 +3,7 @@ class DB{
 
 	private $host = 'localhost';
 	private $username = 'root';
-	private $password = 'requiemmwka';
+	private $password = '';
 	private $database = 'mycart';
 	private $db;
 
